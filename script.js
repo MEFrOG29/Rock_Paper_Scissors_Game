6d11e62bad0cd82920668ac55c414ffbfc7901a4
MEFrOG <vadyasha4@gmail.com>
2026-07-21 13:22:16 +0300

    Create GetComputerChoice function
    I declared the `GetComputerChoice` function, which randomly returns either Rock, Paper, or Scissors.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+//Объявить функцию GetComputerChoice
+//Создать переменную choice
+//Передать в choice рандомное число от 0 до 2 с помощью методов Math.floor и Math.random
+//Прописать условное выражение (тернарный оператор) для трех вариантов: Если choice = 0, то возвращать Rock,
+//Если choice = 1, то возвращать Paper, иначе возвращать Scissors
+function GetComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  choice = 0 ? "Rock" : (choice = 1 ? "Paper" : "Scissors");
+}
