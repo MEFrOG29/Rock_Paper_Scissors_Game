ec3b5969ab0c49c3dd81080b45c0bf260298e06e
MEFrOG <vadyasha4@gmail.com>
2026-07-21 13:40:20 +0300

    Create GetHumanChoice function
    I declared the `GetHumanChoice` function, which accepts a number from the user and outputs their choice

--- script.js
+++ script.js
@@ -5,5 +5,18 @@
 //Если choice = 1, то возвращать Paper, иначе возвращать Scissors
 function GetComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
-  choice = 0 ? "Rock" : (choice = 1 ? "Paper" : "Scissors");
+  return choice == 0 ? "Rock" : choice == 1 ? "Paper" : "Scissors";
 }
+console.log(GetComputerChoice());
+
+//Объявить функцию GetHumanChoice
+//Создать переменную choice
+//С помощью метода prompt() вывести на консоль панель выбора: 1 - Rock, 2 - Paper, 3 - Scissors
+//и запросить у пользователя число от 1 до 3 и передать его переменной choice
+//Прописать условное выражение (тернарный оператор) для трех вариантов: Если choice = 1, то возвращать Rock,
+//Если choice = 2, то возвращать Paper, иначе возвращать Scissors
+function GetHumanChoice() {
+  let choice = prompt(`1 - Rock \n2 - Paper \n3 - Scissors`);
+  return choice == 1 ? "Rock" : choice == 2 ? "Paper" : "Scissors";
+}
+console.log(GetHumanChoice());
